86dd3492f773990ff806af59287f9e5ddcc14e66
braydenrobbins <brayden.robbins@ruralsource.com>
2020-02-21 11:04:06 -0600

    Added the images

--- src/componenets/ProjectHub.js
+++ src/componenets/ProjectHub.js
@@ -1,15 +1,20 @@
 import React, { useContext } from 'react';
 import { ProjectContext } from '../contexts/ProjectContext';
-
+import budget from '../images/budget.png';
+import okcoders from '../images/okcoders.png';
+import vuelo from '../images/vuelo.png';
 const ProjectHub = () => {
     const { projects } = useContext(ProjectContext);
+    const images = { budget, okcoders, vuelo };
     return (
         <div className="container">
+            <h1>My Projects</h1>
             <div className="row row-cols-1 row-cols-md-3">
                 {projects.map(project => {
+                    const image = images[project.imageUrl]
                     return <div className="col mb-4" key={project.title}>
                         <div className="card">
-                            <img src={project.imageUrl} className="card-img-top" alt={project.title} />
+                            <img src={image} className="card-img-top" alt={project.title} />
                             <div className="card-body">
                                 <h5 className="card-title">{project.title}</h5>
                                 <p className="card-text">{project.description}</p>
--- src/contexts/ProjectContext.js
+++ src/contexts/ProjectContext.js
@@ -6,25 +6,25 @@ const ProjectContextProvider = props => {
     const [projects] = useState([
         {
             title: 'Better-Budget',
-            imageUrl: '../images/budget.png',
+            imageUrl: 'budget',
             sourceUrl: 'url',
             description: "My own personal work in progress. I really enjoy budgeting, so I began creating my own budgeting app. MERN Stack."
         },
         {
             title: 'Vuelo',
-            imageUrl: '../images/vuelo.png',
+            imageUrl: 'vuelo',
             sourceUrl: 'url',
             description: "A simple web based Helicopter research site. Pulls from a MongoDB database with a ASP.Net core authentication API, and a Java Springboot backend."
         },
         {
             title: 'OKCoders',
-            imageUrl: '../images/okcoders.png',
+            imageUrl: 'okcoders',
             sourceUrl: 'url',
             description: "A website for OKCoders Alumni to go and add themselves as an alumnus. This was the capstone project at OKCoders built by my peers and I. MERN Stack."
         },
         {
             title: 'Portfolio',
-            imageUrl: 'url',
+            imageUrl: 'portfolio',
             sourceUrl: 'url',
             description: "This Website. A web based representation of my stregths. React front end, with a Node.js Backend."
         },
